0f0322fbec6f5f1ea7e14b11df97870d1ddceabb
kmjyz44 <kmjyz44@gmail.com>
2022-08-23 19:57:25 +0300

    css lesson

--- src/book.js
+++ src/book.js
@@ -15,3 +15,29 @@ export async function getWord (n){
   
   }) 
   }
+ 
+const pagination = document.querySelectorAll('.pagination');
+pagination[0].classList.add('pagination_active');
+
+export function hide_number(){
+  console.log(1);
+   if(pagination[0].classList.contains('pagination_active')){
+    pagination[0].classList.remove('pagination_active');
+    pagination[2].classList.remove('pagination_active');
+    pagination[1].classList.add('pagination_active');
+    
+   }
+    else if(pagination[1].classList.contains('pagination_active')){
+    pagination[1].classList.remove('pagination_active');
+    pagination[0].classList.remove('pagination_active');
+    pagination[2].classList.add('pagination_active');
+   
+   }
+   else
+   if(pagination[2].classList.contains('pagination_active')){
+    pagination[2].classList.remove('pagination_active');
+    pagination[1].classList.remove('pagination_active');
+    pagination[0].classList.add('pagination_active');
+    
+   }
+  }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { getWord} from "./book.js";
+import {hide_number} from './book.js'
 const wrapper_book = document.querySelector('.wrapper_book');
-
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
 
 wrapper_book.addEventListener('click',function (event)  {
   event.stopPropagation(true);
@@ -9,32 +11,7 @@ if(n !== null){
   getWord(n);
 }
 })
-const next = document.querySelector('.next');
-const pagination = document.querySelectorAll('.pagination');
-pagination[0].classList.add('pagination_active');
-
-function hide_number(){
-  console.log(1);
-   if(pagination[0].classList.contains('pagination_active')){
-    pagination[0].classList.remove('pagination_active');
-    pagination[2].classList.remove('pagination_active');
-    pagination[1].classList.add('pagination_active');
-    
-   }
-    else if(pagination[1].classList.contains('pagination_active')){
-    pagination[1].classList.remove('pagination_active');
-    pagination[0].classList.remove('pagination_active');
-    pagination[2].classList.add('pagination_active');
-   
-   }
-   else
-   if(pagination[2].classList.contains('pagination_active')){
-    pagination[2].classList.remove('pagination_active');
-    pagination[1].classList.remove('pagination_active');
-    pagination[0].classList.add('pagination_active');
-    
-   }
-  }
 
 
 next.addEventListener('click', hide_number);
+prev.addEventListener('click', hide_number);
