3391e993a0883c214febe32bbf17fd880d135e6a
kmjyz44 <kmjyz44@gmail.com>
2022-09-04 20:19:43 +0300

    sprint

--- src/sprint.js
+++ src/sprint.js
@@ -1,6 +1,10 @@
 import {getMenu} from "./menu.js"
  getMenu();
+ let arr=[];
  let count = 1;
+ let setinter;
+ const timer_collor = document.querySelector('.timer_collor');
+ const norus = document.querySelector('.norus');
  const start = document.querySelector('.start');
  const timer_sprint = document.querySelector('.timer_sprint');
  const wrapper_book = document.querySelector('.wrapper_book');
@@ -15,17 +19,41 @@ import {getMenu} from "./menu.js"
      }
   })
 
-  function timer (){
-   if(count<9){
-      clearInterval(settimer);
-   let settimer = setInterval (()=>{
-      ++count;
-      timer_sprint.innerHTML=+count;
-   } ,1000);
-   }
+async function getWordSprint(i,n){
+   let out='';
+   let out_word='';
+  await fetch('https://leng-app.herokuapp.com/words?page= '+i+'&group='+n+'')
+   .then((data) => {
+      
+     return data.json();
+   })
+ .then(data => {
+   data.forEach(element => {
+    out = element.wordTranslate;
+    arr.push(out)
+    norus.innerHTML = out;
    
-      clearInterval(settimer);
+   })
+   console.log(arr[6])
+  })
+}
+
+  function timer (){
+   getWordSprint(1,0) ;
+   setinter = setInterval(countSprint,1000);  
+   
+}
+
+
+ function countSprint(){
+   if(count<60){
+   count++;
+      timer_sprint.innerHTML=+count;
+      timer_collor.style.width = count*6+'px';
    }
-  
+   else{
+      clearInterval(11);
+   }
+ }
 start.addEventListener('click', timer);
  
